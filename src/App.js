3981cddafd36a43464200b1f9b1017da14a9feed
Taljjaa <jenken131@gmail.com>
2020-01-09 16:40:29 -0800

    added redux to signup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
    return (
       <Router>
-         <Route exact path="/" render={() => <div>Home</div>} />
+         <Route exact path="/" render={() => <Home />} />
          <Route
             exact
             path="/login"
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Home = () => {
-    
    return <div>Hello welcome to home</div>;
 };
 
