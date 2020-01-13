eb06ebc2cc04cc9f245d6bd66797436460eb6799
Taljjaa <jenken131@gmail.com>
2020-01-13 14:21:23 -0800

    conditionally render button based on member status

--- src/components/MemberOptions.js
+++ src/components/MemberOptions.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const MemberOptions = () => {
-   return <div>Member</div>;
+   const users = useSelector(state => state.bookclub.bookclub.users);
+   const currentUser = useSelector(state => state.auth.user.id);
+   console.log(users);
+
+   const isMember = () => {
+      let temp = users.filter(user => {
+         return user.id === currentUser;
+      });
+      if (temp.length > 0) {
+         return true;
+      }
+      return false;
+   };
+
+   return (
+      <div>
+         {isMember() ? (
+            <button>Leave Bookclub</button>
+         ) : (
+            <button>Join Bookclub</button>
+         )}
+      </div>
+   );
 };
 
 export default MemberOptions;
