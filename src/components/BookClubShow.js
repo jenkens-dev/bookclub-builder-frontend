d100f060c400d34a042a15f61e67ddd347cba05c
Taljjaa <jenken131@gmail.com>
2020-01-13 11:12:37 -0800

    added useEffect to determine if currentUser is member or not

--- src/components/BookClubShow.js
+++ src/components/BookClubShow.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { api } from '../services/api';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const BookClubShow = ({ props, match }) => {
-   const user = useSelector(state => state.auth.user.id);
+   const [bookclubs, setBookclubs] = useState('');
+   const [fetched, setFetched] = useState(false);
+   const [members, setMembers] = useState([]);
+   const [isAdmin, setAdmin] = useState(false);
+   const currentUser = useSelector(state => state.auth.user.id);
    const history = useHistory();
    const token = localStorage.getItem('token');
 
-   console.log(user);
+   //    console.log(bookClubDetails.id);
+   //       console.log(currentUser);
+   //       let members = users.map(user => {
+   //          return user.id;
+   //       });
+   //       console.log(members.includes(currentUser));
 
-   if (!token || !user) {
+   useEffect(() => {
+      fetch(`http://localhost:3000/api/v1/bookclubs/${match.params.id}`)
+         .then(response => response.json())
+         .then(data => {
+            setBookclubs(data);
+            setMembers(data.users);
+         })
+         .finally(() => {
+            console.log(members);
+            // console.log(isMemberBoolean);
+            // if (isMemberBoolean.length > 0) {
+            //    setMember(true);
+            // } else {
+            //    setMember(false);
+            // }
+            setFetched(true);
+         });
+   }, []);
+
+   if (!token || !currentUser) {
       history.push('/login');
    }
-
-   const [bookClubDetails, fetched] = api.useNetworkResource(
-      `http://localhost:3000/api/v1/bookclubs/${match.params.id}`,
-   );
 
    if (!fetched) {
       return null;
    }
 
    const handleClick = () => {
-      console.log(bookClubDetails.id);
-      console.log(user);
+      console.log('hi');
    };
 
-   console.log(bookClubDetails);
-   const { name, description, picture } = bookClubDetails;
+   const isMember = () => {
+      let temp = members.filter(member => member.id === currentUser);
+      if (temp.length > 0) {
+         return true;
+      } else {
+         return false;
+      }
+   };
+
+   console.log(bookclubs);
+   const { name, description, picture } = bookclubs;
+   console.log(members);
    return (
       <div>
+         <h1>{isMember() ? <div>Member</div> : <div>No</div>}</h1>
          <h1>{name}</h1>
          <button onClick={handleClick}>Join</button>
          <img src={picture} />
