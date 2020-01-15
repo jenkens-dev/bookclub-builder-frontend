42d4d3a8476f14f845128dbd83532735b3517a33
Taljjaa <jenken131@gmail.com>
2020-01-15 11:20:37 -0800

    added form to poll options and can successfully fetch from google books api

--- src/components/PollOptions.js
+++ src/components/PollOptions.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const PollOptions = () => {
    const poll = useSelector(state => state.poll.poll);
-   return <div>Hi</div>;
+   const [select, setSelect] = useState('inauthor');
+   const [search, setSearch] = useState('');
+
+   const handleSubmit = event => {
+      event.preventDefault();
+      fetch(
+         `https://www.googleapis.com/books/v1/volumes?q=${select}:${search}&maxResults=6&key=AIzaSyA39gVxErny8CXc27ySrZefMllAWF_det4`,
+      )
+         .then(response => response.json())
+         .then(data => {
+            console.log(data);
+         });
+   };
+
+   const handleSearchChange = event => {
+      setSearch(event.target.value);
+   };
+
+   const handleSelectChange = event => {
+      setSelect(event.target.value);
+   };
+
+   return (
+      <div>
+         <h1>{poll.name}</h1>
+         <form onSubmit={handleSubmit}>
+            <label>
+               Search
+               <input
+                  type="text"
+                  value={search}
+                  onChange={handleSearchChange}
+               />
+               <select onChange={handleSelectChange}>
+                  <option value="inauthor">Author</option>
+                  <option value="intitle">Title</option>
+               </select>
+               <input type="submit" />
+            </label>
+         </form>
+      </div>
+   );
 };
 
 export default PollOptions;
