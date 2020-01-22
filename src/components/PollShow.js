8312ba8d48e6c9cba55f0a04ba9423afbf5d34a8
Taljjaa <jenken131@gmail.com>
2020-01-21 17:17:13 -0800

    added outline to poll container

--- src/components/PollShow.js
+++ src/components/PollShow.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import PollVote from './PollVote';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 const PollShow = () => {
+   const useStyles = makeStyles({
+      poll: {
+         border: '1px solid gray',
+         borderRadius: 5,
+         width: '55%',
+      },
+   });
+
+   const classes = useStyles();
    const poll = useSelector(state => state.poll.poll);
 
    const displayPollOptions = () => {
@@ -13,7 +23,13 @@ const PollShow = () => {
    };
 
    return (
-      <Grid container direction="column" justify="center" alignItems="center">
+      <Grid
+         container
+         direction="column"
+         justify="center"
+         alignItems="center"
+         className={classes.poll}
+      >
          <h1>{poll.name}</h1>
          {poll.poll_options.length > 0 && <>{displayPollOptions()}</>}
       </Grid>
