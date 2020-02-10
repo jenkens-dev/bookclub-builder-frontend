f0b8b2c8eb1836c11a7b88c41e9333ddbe44bc68
Taljjaa <jenken131@gmail.com>
2020-02-10 13:15:28 -0800

    Updates

---
+++ precache-manifest.84d481e4dcd2285e665258cb3b1b1edd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dde471f2dae638e5290a956620f528",
+    "url": "/bookclub-builder-frontend/index.html"
+  },
+  {
+    "revision": "7aab9a3143b5511d3d01",
+    "url": "/bookclub-builder-frontend/static/css/2.df1c6120.chunk.css"
+  },
+  {
+    "revision": "a903b7cc242b9802fb79",
+    "url": "/bookclub-builder-frontend/static/css/main.622488f0.chunk.css"
+  },
+  {
+    "revision": "7aab9a3143b5511d3d01",
+    "url": "/bookclub-builder-frontend/static/js/2.3b8daad1.chunk.js"
+  },
+  {
+    "revision": "c6a5a8a217bba9f5fd4171cf2cbaf049",
+    "url": "/bookclub-builder-frontend/static/js/2.3b8daad1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a903b7cc242b9802fb79",
+    "url": "/bookclub-builder-frontend/static/js/main.556e85c9.chunk.js"
+  },
+  {
+    "revision": "4a9ba1714c0002da142f",
+    "url": "/bookclub-builder-frontend/static/js/runtime-main.b2d5f707.js"
+  },
+  {
+    "revision": "20cf9821a374d5764711e9365a99ec6d",
+    "url": "/bookclub-builder-frontend/static/media/logo.20cf9821.png"
+  }
+]);
